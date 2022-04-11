d12f6ad28d214b99b3774490549cdba77b048bbe
TileRunner <justchrissykes@gmail.com>
2022-04-11 16:47:13 -0400

    Add my code in from trcode
    Adjusted as needed.

--- tm/src/App.js
+++ tm/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import mylogo from './transmogrify.jpg';
 import './App.css';
+import './tm.css';
+import './trcss.css';
+import { useState } from 'react';
+import Transmogrify from './tm';
 
 function App() {
+  const [whereTo, setWhereTo] = useState('home');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {whereTo === 'home' ?
+        <header>
+          <div className="App-header">
+            <img src={mylogo} className="App-logo" alt="logo" />
+          </div>
+          <div className='App-under-logo'>
+            <p>An original word game by Tile Runner</p>
+            <p>Make one move at a time to transmogrify to target.</p>
+            <button onClick={() => {setWhereTo('game');}}>Play !</button>
+          </div>
+        </header>
+      :
+        <header>
+          <Transmogrify setWhereTo={setWhereTo}/>
+        </header>
+      }
     </div>
   );
 }
---
+++ tm/src/wordfunctions.js
@@ -0,0 +1,95 @@
+const baseurl = 'https://tilerunner.herokuapp.com';
+
+/**
+ * Get the alphagram for the given word, trimmed and converted to lower case.
+ * @param {string} word A word that you want the alphagram for, trimmed and converted to lower case.
+ * @returns {string} The alphagram
+ */
+function getAlphagram(word="") {
+    let a = Array.from(word.trim().toLowerCase());
+    a.sort();
+    let alphagram = a.join('');
+    return alphagram;
+}
+
+/**
+ * Determine how many swaps between two words of equal length, case insensitive.
+ * @param {string} word1 A word
+ * @param {string} word2 Another word of the same length
+ * @returns {number} The number of positions at which the letter is different between the two words, -1 if unequal length words.
+ */
+export function countSwaps(word1="", word2="") {
+    let result = 0;
+    let letters1 = Array.from(word1.toLowerCase());
+    let letters2 = Array.from(word2.toLowerCase());
+    if (letters1.length === letters2.length) {
+        for (let i = 0; i < letters1.length; i++) {
+            if (letters1[i] !== letters2[i]) {
+                result++;
+            }
+        }
+    } else {
+        result = -1;
+    }
+    return result;
+}
+
+/**
+ * Determine whether two words are anagrams, case insensitive. If they are the same word it will still return true.
+ * @param {string} word1 A word
+ * @param {string} word2 Another word
+ * @returns {bool} Whether the two words are anagrams.
+ */
+export function areAnagrams(word1="", word2="") {
+    let alphagram1 = getAlphagram(word1);
+    let alphagram2 = getAlphagram(word2);
+    return alphagram1 === alphagram2;
+}
+
+/**
+ * Determine where dropping any one letter from the longer word forms the shorter word, case insensitive
+ * @param {string} longword The longer word
+ * @param {string} shortword The shorter word
+ * @returns {bool} Whether you can drop a letter from the longer word to get the shorter word
+ */
+export function isDrop(longword="", shortword="") {
+    if (longword.length !== shortword.length + 1) {
+        return false; // Must be 1 fewer letter to be a drop
+    }
+    let lw = longword.toLowerCase();
+    let sw = shortword.toLowerCase();
+    for (let i = 0; i < lw.length; i++) {
+        let cw = Array.from(lw);
+        cw.splice(i,1);
+        if (cw.join('') === sw) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Determine whether a word is in the ENABLE2K lexicon, case insensitive
+ * @param {string} word A word
+ * @returns {Promise<bool>} Whether the word is in the ENABLE2K lexicon
+ * @async
+ */
+export async function isWordValid(word) {
+    let url = `${baseurl}/ENABLE2K?exists=${word.toLowerCase()}`;
+    const response = await fetch(url);
+    const jdata = await response.json();
+    return jdata.exists;
+}
+
+/**
+ * Determine valid next words per transmogrify rules
+ * @param {string} word The word being moved from, case insensitive
+ * @returns {Promise<string[]>} The string array of valid words to move to, in lower case
+ * @async
+ */
+export async function getTransmogrifyValidNextWords(word) {
+    let url = `${baseurl}/ENABLE2K?letters=${word.toLowerCase()}&tm=true`;
+    const response = await fetch(url);
+    const jdata = await response.json();
+    return jdata.to;
+}
